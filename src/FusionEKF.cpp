d27639a2e4fd8595fbe7a85f05350ef513790a95
eric van tassell <Eric.VanTassell@amd.com>
2019-01-04 21:10:28 -0600

    init state of ekf_.x for radar

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -67,9 +67,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      // TODO: Convert radar from polar to cartesian coordinates 
+      // TODO: Convert radar from polar to cartesian coordinates <done>
       //         and initialize state.
-      assert("port_bomb" == 0);
+      // https://www.mathsisfun.com/polar-cartesian-coordinates.html
+      // x = r × cos( θ )
+      // y = r × sin( θ )
+      float ro       = measurement_pack.raw_measurements_(0);
+      float phi      = measurement_pack.raw_measurements_(1);
+      float ro_dot = measurement_pack.raw_measurements_(2);
+      
+      ekf_.x_(0) = ro * cos(phi);
+      ekf_.x_(1) = ro * sin(phi);      
+      ekf_.x_(2) = ro_dot * cos(phi);
+      ekf_.x_(3) = ro_dot * sin(phi);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.<done>
