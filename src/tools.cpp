2d5b4cf75f58c12f772030b8be08dc63879874b8
eric van tassell <Eric.VanTassell@amd.com>
2019-01-04 22:35:11 -0600

    Tools:CalculateRMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -6,6 +6,8 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -14,9 +16,40 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-   * TODO: Calculate the RMSE here.
+   * TODO: Calculate the RMSE here.<done>
    */
-  assert("port_bomb" == 0);
+  // per Lesson 25, Unit 23 Evaluating KF Performance Part 1 calculate rmse
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  //<hit>assert("port_bomb" == 0);
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() != ground_truth.size()
+      || estimations.size() == 0){
+      std::cout << "Invalid estimation or ground_truth data" << std::endl;
+      return rmse;
+  }
+
+  //accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i){
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+
+      //coefficient-wise multiplication
+      residual = residual.array() * residual.array();
+      rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse / estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
